6ffcfa51b0b21b63d5961727b2bb97ff604a5880
Necron000 <ardaocalan@gmail.com>
2026-08-25 15:27:32 +0300

    Delete iyzico.project directory

--- iyzico.project/src/main/java/com/arda/iyzico/project/exceptions/InsufficientStockException.java
+++
@@ -1,7 +0,0 @@
-package com.arda.iyzico.project.exceptions;
-
-public class InsufficientStockException extends RuntimeException {
-    public InsufficientStockException(Long itemId, int available, int requested) {
-        super("Item " + itemId + " has " + available + " in stock, " + requested + " requested.");
-    }
-}
--- iyzico.project/src/main/java/com/arda/iyzico/project/models/PaymentStatus.java
+++
@@ -1,9 +0,0 @@
-package com.arda.iyzico.project.models;
-
-public enum PaymentStatus {
-    PENDING,
-    QUEUED,
-    APPROVED,
-    REJECTED,
-    FAILED
-}
